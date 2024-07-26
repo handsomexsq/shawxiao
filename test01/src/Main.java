bddbfe0d4156199898f4f164262598d27e0e3c03
shawxiao <1922552680@qq.com>
2024-07-26 21:49:45 +0800

    在idea中对分支test02进行修改

--- test01/src/Main.java
+++ test01/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("你好呀");
+        System.out.println("你好呀 这里是01分支 idea");
+        System.out.println("你好呀 这里是02分支 idea");
     }
 }
